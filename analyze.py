f2016ac8354ecdb62ad85bd24167fb09ee6cb83b
marcverhagen <marc@cs.brandeis.edu>
2017-02-18 16:37:49 -0500

    Cleaned up analysis script.

--- analyze.py
+++ analyze.py
@@ -1,21 +1,46 @@
+"""analyze.py
+
+Some basic analysis of the predicates in VerbNet.
+
+Usage:
+
+   $ python analyze.py [--verbnet VERBNET_FOLDER] [--predicate PRED_NAME]
+
+VERBNET_FOLDER is the location of VerbNet and PRED_NAME is the kind of predicate
+to analyze. Both arguments are optional, the defaults are in the VERBNET and
+PRED variables. If --predicate is not used than PRED will be None and no
+restrictions will be used.
+
+Examples:
+
+   $ python analyze.py
+   $ python analyze.py --predicate motion
+
+Results are written to two files:
+
+   out-missing-roles.html
+   out-predicates.txt
+
 """
 
-Analyzing the predicates in VerbNet.
-
-Set VERBNET to the location of VerbNet and PRED to restrict statistics to those
-classes that contain that predicate, use None if no restriction is desired.
-
-"""
-
-
+import sys, getopt
 from verbnet import VerbNet, PredicateStatistics
 
-VERBNET = "/Users/marc/Dropbox/sift/cwc/verbnet/3.2/new_vn"
 
+VERBNET = '/DATA/resources/lexicons/verbnet/verbnet-3.3'
 PRED = None
-PRED = 'motion'
 
-vn = VerbNet(directory=VERBNET)
-stats = PredicateStatistics(vn, PRED)
-stats.print_missing_links('out-missing-roles.html')
-stats.print_predicates('out-predicates.txt')
+
+if __name__ == '__main__':
+
+    (opts, args) = getopt.getopt(sys.argv[1:], '', ['verbnet=', 'predicate='])
+    for opt, arg in opts:
+        if opt == '--verbnet':
+            VERBNET = arg
+        elif opt == '--predicate':
+            PRED = arg
+    dir = sys.argv[1] if len(sys.argv) > 1 else VERBNET
+    vn = VerbNet(directory=VERBNET)
+    stats = PredicateStatistics(vn, PRED)
+    stats.print_missing_links('out-missing-roles.html')
+    stats.print_predicates('out-predicates.txt')
